f04fe08bd6603b162fc2c8e375d9ddc3096d13dc
shaig-mahmudov <zyphorix.official@gmail.com>
2026-05-07 06:46:34 -0700

    add: data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO docker_app_entity (name)
+VALUES ('Docker Test 1');
+
+INSERT INTO docker_app_entity (name)
+VALUES ('Docker Test 2');
+
+INSERT INTO docker_app_entity (name)
+VALUES ('Hello from Docker DB');
+
+INSERT INTO docker_app_entity (name)
+VALUES ('Spring Boot + MySQL working!');
